9b6dfb8efac9b9b3aab2f975f7f67945fa058a58
Techdudetony <AALee2@student.fullsail.edu>
2024-10-17 11:09:30 -0400

    Updated layout and UI of the Sign Up page and Login page. Next steps: add visual feedback to buttons and link to database.

--- app/src/main/java/com/example/project_aura_bloom/SignUpFragment.kt
+++ app/src/main/java/com/example/project_aura_bloom/SignUpFragment.kt
@@ -16,7 +16,7 @@ class SignUpFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentSignUpBinding.inflate(inflater, container, false)
         return binding.root
     }
