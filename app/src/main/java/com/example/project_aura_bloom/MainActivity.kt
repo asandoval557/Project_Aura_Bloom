061a25cc939080eb0c4bf139934f53d01ea09492
ChanChanCU <chantelly2014@gmail.com>
2024-10-24 06:53:11 -0700

    Moved icons to side drawer menu and removed bottom nav

--- app/src/main/java/com/example/project_aura_bloom/MainActivity.kt
+++ app/src/main/java/com/example/project_aura_bloom/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
@@ -25,6 +26,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var navView: NavigationView
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,14 +36,24 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
-            // Setup for drawer and nav view for the side menu
+        // Setup for drawer and nav view for the side menu
         drawerLayout = findViewById(R.id.drawer_layout)
         navView = findViewById(R.id.nav_view)
 
-            // Setting up nav for switch management between screens
+        // Setting up nav for switch management between screens
         val navController = findNavController(R.id.nav_host_fragment_content_main)
 
-            // Forming the app bar to work with the drawer
+        // Toggle button for closing and opening menu drawer
+        toggle = ActionBarDrawerToggle(
+            this, drawerLayout, binding.toolbar,
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+
+        // Syncing for the toggle state and menu drawer state
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        // Forming the app bar to work with the drawer
         appBarConfiguration = AppBarConfiguration(setOf(R.id.HomeScreenFragment,
                                                         R.id.CalmZoneFragment,
                                                         R.id.MoodProgressFragment), drawerLayout)
@@ -49,21 +61,33 @@ class MainActivity : AppCompatActivity() {
 
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-            // Setup for the bottom nav for easy switch between screens
-        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_nav)
-        bottomNavView.setupWithNavController(navController)
 
-            // Toggle button for closing and opening menu drawer
-        val toggle = ActionBarDrawerToggle(
-            this, drawerLayout, binding.toolbar,
-            R.string.navigation_drawer_open, R.string.navigation_drawer_close
-        )
 
-            // Disabled the default menu icon and set to the hot dog icon
-        toggle.isDrawerIndicatorEnabled = false
-        binding.toolbar.setNavigationIcon(R.drawable.ic_hotdog_menu)
+            // Display hotdog menu as default icon for side drawer and syncing when returning to home screen
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.HomeScreenFragment) {
+                // Only show hotdog menu icon on home screen
+                toggle.isDrawerIndicatorEnabled = true
+                binding.toolbar.setNavigationIcon(R.drawable.ic_hotdog_menu)
+                binding.toolbar.setNavigationOnClickListener {
+                    if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                        drawerLayout.closeDrawer(GravityCompat.START)
+                    } else {
+                        drawerLayout.openDrawer(GravityCompat.START)
+                    }
+                }
+            } else {
+                // Show back arrow on fragment screens
+                toggle.isDrawerIndicatorEnabled = false
+                supportActionBar?.setDisplayShowTitleEnabled(true)
+                binding.toolbar.setNavigationIcon(R.drawable.ic_back_arrow)
+                binding.toolbar.setNavigationOnClickListener {
+                    navController.navigateUp()
+                }
+            }
+        }
 
-            // Setting up the click event for the hot dog menu
+        // Setting up the click event for the hot dog menu
         binding.toolbar.setNavigationOnClickListener {
             if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
                 drawerLayout.closeDrawer(GravityCompat.START)
@@ -72,45 +96,33 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-            // Syncing for the toggle state and menu drawer state
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
 
-            // When Statement for bottom nav button click
-        bottomNavView.setOnNavigationItemSelectedListener { item ->
+
+        // When Statement for bottom nav button click
+        navView.setNavigationItemSelectedListener { item ->
             when (item.itemId) {
-
-                    // If mood meter is clicked then...
-                R.id.nav_mood_meter -> {
-                        // Screen switches to the (mood) progress screen
+                R.id.nav_mood_meter ->{
                     navController.navigate(R.id.MoodProgressFragment)
+                    drawerLayout.closeDrawer(GravityCompat.START)
                     true
                 }
-
                 R.id.nav_resources -> {
-
                     true
                 }
-
                 R.id.nav_help -> {
-
                     true
                 }
-
                 R.id.nav_exercise -> {
-
                     true
                 }
-
-                    // If calm zone button is clicked then...
                 R.id.nav_calm_zone -> {
-                        // Screen switches to the calm zones main hub
                     navController.navigate(R.id.CalmZoneFragment)
+                    drawerLayout.closeDrawer(GravityCompat.START)
                     true
                 }
-
                 else -> false
             }
+
         }
 
     }
--- app/src/main/java/com/example/project_aura_bloom/MoodProgressFragment.kt
+++ app/src/main/java/com/example/project_aura_bloom/MoodProgressFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
 class MoodProgressFragment : Fragment() {
 
