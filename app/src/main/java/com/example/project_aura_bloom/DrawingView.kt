46c75466fe86188066b39114c21f844a167db6a4
James Ellis <jmellis1@student.fullsail.edu>
2024-10-25 18:06:34 -0400

    added strings, redid some icons, created toolbar and drawing space

---
+++ app/src/main/java/com/example/project_aura_bloom/DrawingView.kt
@@ -0,0 +1,93 @@
+package com.example.project_aura_bloom
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+
+class DrawingView constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+   private val paint = Paint().apply {
+      color = Color.BLACK
+      style = Paint.Style.STROKE
+      strokeWidth = 10f
+      isAntiAlias = true
+   }
+
+    private val path = Path()
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawPath(path, paint)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        val x = event.x
+        val y = event.y
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN, MotionEvent
+                .ACTION_POINTER_DOWN -> {
+                path.moveTo(x, y)
+                invalidate()
+                return true
+            }
+            MotionEvent.ACTION_MOVE -> {
+                val pressure = event.pressure
+                val pointerCount = event.pointerCount
+
+                paint.strokeWidth = 10f * pressure
+
+                for (i in 0 until pointerCount) {
+                    val px = event.getX(i)
+                    val py = event.getY(i)
+                    path.lineTo(px, py)
+                }
+                invalidate()
+            }
+            MotionEvent.ACTION_UP -> {
+                // finalize the path
+                path.lineTo(x, y)
+                invalidate()
+            }
+            else -> return false
+        }
+
+        return true
+    }
+
+    fun setEraseMode() {
+        paint.color = Color.WHITE
+    }
+
+    fun undo() {
+        TODO("Not yet implemented")
+    }
+
+    fun redo() {
+        TODO("Not yet implemented")
+    }
+
+    fun setBrushSize(fl: Float) {
+        paint.strokeWidth = fl
+    }
+
+    fun setBrushColor(color: Int) {
+        paint.color = color
+    }
+
+    fun saveDrawing() {
+        TODO("Not yet implemented")
+    }
+
+    fun shareDrawing() {
+        TODO("Not yet implemented")
+    }
+
+
+}
--- app/src/main/java/com/example/project_aura_bloom/PeacefulCreationsFragment.kt
+++ app/src/main/java/com/example/project_aura_bloom/PeacefulCreationsFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.project_aura_bloom.databinding.PeacefulCreationsHubBinding
 
 class PeacefulCreationsFragment : Fragment() {
@@ -17,6 +18,14 @@ class PeacefulCreationsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = PeacefulCreationsHubBinding.inflate(inflater,container,false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.mindfulArt.setOnClickListener {
+            // Screen switches to the mindful art screen
+            findNavController().navigate(R.id.action_PeacefulCreationsFragment_to_MindfulArt)
+        }
     }
 
         // Binding clean up when view is destroyed to avoid memory leaks
